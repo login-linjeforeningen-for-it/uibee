10b190cb7e6f29450fa52ac4ffac8804165bea2f
eirikhanasand <eirik.hanasand@gmail.com>
2026-03-24 20:35:22 +0100

    rebuild for 2.14.0

--- dist/src/components/inputs/shared/fieldWrapper.js
+++ dist/src/components/inputs/shared/fieldWrapper.js
@@ -2,6 +2,7 @@ import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import InputLabel from './inputLabel';
 import InputInfo from './inputInfo';
 import InputError from './inputError';
+import ReactMarkdown from 'react-markdown';
 export default function FieldWrapper({ label, name, required, info, error, description, textSize = 'sm', children, className, }) {
-    return (_jsxs("div", { className: `flex flex-col gap-1 w-full relative ${className || ''}`, children: [(label || info) && (_jsxs("div", { className: 'flex items-center justify-between mb-1', children: [label && (_jsx(InputLabel, { label: label, name: name, required: required, className: `ml-1 ${textSize === 'sm' ? 'text-sm!' : 'text-base!'}` })), info && _jsx(InputInfo, { info: info })] })), description && (_jsx("p", { className: 'text-sm text-login-100 ml-1 mb-1', children: description })), children, _jsx(InputError, { error: error, id: `${name}-error` })] }));
+    return (_jsxs("div", { className: `flex flex-col gap-1 w-full relative ${className || ''}`, children: [(label || info) && (_jsxs("div", { className: 'flex items-center justify-between mb-1', children: [label && (_jsx(InputLabel, { label: label, name: name, required: required, className: `ml-1 ${textSize === 'sm' ? 'text-sm!' : 'text-base!'}` })), info && _jsx(InputInfo, { info: info })] })), description && (_jsx("p", { className: 'text-sm text-login-100 ml-1 mb-1', children: _jsx(ReactMarkdown, { children: String(description || '') }) })), children, _jsx(InputError, { error: error, id: `${name}-error` })] }));
 }
