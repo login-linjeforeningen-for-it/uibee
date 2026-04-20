fdc2f2063d4eaa425353fe7b9ca17b5d7f84a494
0herm <0herm@protonmail.com>
2026-04-20 20:23:26 +0200

    v2.16.26 fix proto

--- dist/src/utils/auth/getDomain.js
+++ dist/src/utils/auth/getDomain.js
@@ -1,5 +1,6 @@
 export function getDomain(req) {
-    const proto = req.headers.get('x-forwarded-proto') ?? new URL(req.url).protocol.replace(':', '');
-    const host = req.headers.get('x-forwarded-host') ?? req.headers.get('host') ?? new URL(req.url).host;
+    const clientProto = req.headers.get('x-forwarded-proto');
+    const proto = clientProto ?? new URL(req.url).protocol.replace(':', '');
+    const host = req.headers.get('host') ?? req.headers.get('x-forwarded-host') ?? new URL(req.url).host;
     return `${proto}://${host}`;
 }
--- src/utils/auth/getDomain.ts
+++ src/utils/auth/getDomain.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from 'next/server'
 
 export function getDomain(req: NextRequest): string {
-    const proto = req.headers.get('x-forwarded-proto') ?? new URL(req.url).protocol.replace(':', '')
-    const host = req.headers.get('x-forwarded-host') ?? req.headers.get('host') ?? new URL(req.url).host
+    const clientProto = req.headers.get('x-forwarded-proto')
+    const proto = clientProto ?? new URL(req.url).protocol.replace(':', '')
+    const host = req.headers.get('host') ?? req.headers.get('x-forwarded-host') ?? new URL(req.url).host
+
     return `${proto}://${host}`
 }
