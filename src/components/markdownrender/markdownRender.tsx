fc2c420c90fdc67b9794f1ef84f03aa0210ebb72
0herm <0herm@protonmail.com>
2026-05-14 20:40:37 +0200

    add rehype-highlight for syntax highlighting

--- src/components/markdownrender/markdownRender.tsx
+++ src/components/markdownrender/markdownRender.tsx
@@ -1,8 +1,17 @@
 import Markdown, { Components } from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/github-dark.css'
 
 const defaultComponents: Components = {
     h1: ({...props}) => <h2 {...props} />,
+    pre({ children }) {
+        return (
+            <pre className='inline-block rounded-lg overflow-auto whitespace-pre-wrap wrap-break-word w-full'>
+                {children}
+            </pre>
+        )
+    },
 }
 
 export default function MarkdownRender({ MDstr, components, className }: {
@@ -15,6 +24,7 @@ export default function MarkdownRender({ MDstr, components, className }: {
             <Markdown
                 components={{ ...defaultComponents, ...components }}
                 remarkPlugins={[remarkGfm]}
+                rehypePlugins={[rehypeHighlight]}
             >
                 {MDstr.replace(/\\n/g, '\n')}
             </Markdown>
