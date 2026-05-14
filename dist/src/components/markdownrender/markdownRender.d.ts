d7f6f784ae3847ca2b9ae43e41fe50ea6a2272e5
0herm <0herm@protonmail.com>
2026-05-14 20:59:03 +0200

    build

--- dist/src/components/markdownrender/markdownRender.d.ts
+++ dist/src/components/markdownrender/markdownRender.d.ts
@@ -1,4 +1,5 @@
 import { Components } from 'react-markdown';
+import 'highlight.js/styles/github-dark.css';
 export default function MarkdownRender({ MDstr, components, className }: {
     MDstr: string;
     components?: Components;
--- dist/src/components/markdownrender/markdownRender.js
+++ dist/src/components/markdownrender/markdownRender.js
@@ -1,9 +1,14 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import 'highlight.js/styles/github-dark.css';
 const defaultComponents = {
     h1: ({ ...props }) => _jsx("h2", { ...props }),
+    pre({ children }) {
+        return (_jsx("pre", { className: 'inline-block rounded-lg overflow-auto whitespace-pre-wrap wrap-break-word w-full', children: children }));
+    },
 };
 export default function MarkdownRender({ MDstr, components, className }) {
-    return (_jsx("div", { className: className ?? 'prose prose-sm prose-custom max-w-none', children: _jsx(Markdown, { components: { ...defaultComponents, ...components }, remarkPlugins: [remarkGfm], children: MDstr.replace(/\\n/g, '\n') }) }));
+    return (_jsx("div", { className: className ?? 'prose prose-sm prose-custom max-w-none', children: _jsx(Markdown, { components: { ...defaultComponents, ...components }, remarkPlugins: [remarkGfm], rehypePlugins: [rehypeHighlight], children: MDstr.replace(/\\n/g, '\n') }) }));
 }
