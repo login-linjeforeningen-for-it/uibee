6757c5e43cfc594aa811a40f372b9e67337bd7f2
0herm <0herm@protonmail.com>
2026-05-14 20:08:00 +0200

    build

--- dist/src/components/markdownrender/markdownRender.d.ts
+++ dist/src/components/markdownrender/markdownRender.d.ts
@@ -1,3 +1,6 @@
-export default function MarkdownRender({ MDstr }: {
+import { Components } from 'react-markdown';
+export default function MarkdownRender({ MDstr, components, className }: {
     MDstr: string;
+    components?: Components;
+    className?: string;
 }): import("react/jsx-runtime").JSX.Element;
--- dist/src/components/markdownrender/markdownRender.js
+++ dist/src/components/markdownrender/markdownRender.js
@@ -1,6 +1,9 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-export default function MarkdownRender({ MDstr }) {
-    return (_jsx("div", { className: 'prose prose-sm prose-custom max-w-none', children: _jsx(Markdown, { components: { h1: ({ ...props }) => _jsx("h2", { ...props }) }, remarkPlugins: [remarkGfm], children: MDstr.replace(/\\n/g, '\n') }) }));
+const defaultComponents = {
+    h1: ({ ...props }) => _jsx("h2", { ...props }),
+};
+export default function MarkdownRender({ MDstr, components, className }) {
+    return (_jsx("div", { className: className ?? 'prose prose-sm prose-custom max-w-none', children: _jsx(Markdown, { components: { ...defaultComponents, ...components }, remarkPlugins: [remarkGfm], children: MDstr.replace(/\\n/g, '\n') }) }));
 }
